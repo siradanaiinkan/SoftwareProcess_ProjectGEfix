76fc033dc1ec6d0d46f04a3d760059c2e4ab78f1
aoomUwU <119989552+aoomUwU@users.noreply.github.com>
2023-09-25 01:17:01 +0700

    Add files via upload

---
+++ GePlus/dashboard.js
@@ -0,0 +1,12 @@
+// JavaScript สำหรับเปิด Pop-up โดยอัตโนมัติ
+openPopup();
+
+// JavaScript สำหรับเปิด Pop-up
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+
+// JavaScript สำหรับปิด Pop-up
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
