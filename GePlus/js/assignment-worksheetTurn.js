39602b64d723a70fb9865f694bd23ced3984ff47
aoomUwU <119989552+aoomUwU@users.noreply.github.com>
2023-09-26 12:50:12 +0700

    Delete GePlus directory

--- GePlus/js/assignment-worksheetTurn.js
+++
@@ -1,9 +0,0 @@
-// JavaScript สำหรับเปิด Pop-up
-function openPopup() {
-    document.getElementById("popup").style.display = "block";
-}
-
-// JavaScript สำหรับปิด Pop-up
-function closePopup() {
-    document.getElementById("popup").style.display = "none";
-}
